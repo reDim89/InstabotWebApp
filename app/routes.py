3313530e68f9b034aa45138264d4b8c3516e74bf
reDim89 <idmitat@gmail.com>
2017-09-20 22:28:29 +0300

    changed path to static folder

--- app/routes.py
+++ app/routes.py
@@ -7,5 +7,5 @@ def setup_routes(app):
     app.router.add_get('/mybot', mybot)
     app.router.add_post('/mybot', show_log)
     app.router.add_static('/static',
-                          path=str('/static'),
+                          path=str('static'),
                           name='static')
