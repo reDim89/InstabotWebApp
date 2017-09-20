0ce16645ffdfa692b9d86eb781653a85498c0d71
reDim89 <idmitat@gmail.com>
2017-09-20 22:27:04 +0300

    changed path to static folder

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,5 @@
 from views import index, login, mybot, show_log
 
-project_root = '/Users/reDim89/Desktop/Code/InstabotWebApp/'
-
 
 def setup_routes(app):
     app.router.add_get('/', index)
@@ -9,5 +7,5 @@ def setup_routes(app):
     app.router.add_get('/mybot', mybot)
     app.router.add_post('/mybot', show_log)
     app.router.add_static('/static',
-                          path=str(project_root + 'static'),
+                          path=str('/static'),
                           name='static')
